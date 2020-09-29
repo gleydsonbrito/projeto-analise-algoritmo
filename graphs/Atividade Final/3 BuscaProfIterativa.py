4590fbf0bc9c303de22703c79cd790111541e32e
gleydsonbrito <gleydsonbrito@hotmail.com>
2020-09-28 22:16:27 -0300

    added dfsIteractive

---
+++ graphs/Atividade Final/3 BuscaProfIterativa.py
@@ -0,0 +1,25 @@
+G = {'A': set(['B', 'C']),
+     'B': set(['A', 'D', 'E']),
+     'C': set(['A', 'F']),
+     'D': set(['B']),
+     'E': set(['B', 'F']),
+     'F': set(['C', 'E'])}
+
+
+def dfs(G, s):
+    print('Inicia pelo vertice {}'.format(s))
+    visitado = set()
+    print('Empilha {}'.format(s))
+    P = [s]
+    while len(P) > 0:
+        vertice = P.pop()
+        print('Desempilha {}'.format(vertice))
+        if vertice not in visitado:
+            visitado.add(vertice)
+            print('Visita o vétice {}'.format(vertice))
+            P.extend(G[vertice] - visitado)
+            print('Tira {} do grafo G'.format(vertice))
+    return visitado
+
+
+print('Resultado: ', dfs(G, 'A'))
