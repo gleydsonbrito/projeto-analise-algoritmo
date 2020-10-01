c6b3d9c22e5ff5eb1a8ac02352bfa48ba96b077f
gleydsonbrito <gleydsonbrito@hotmail.com>
2020-10-01 10:20:34 -0300

    added dijkstra heap

--- graphs/Atividade Final/17 DijkstraHeap.py
+++ graphs/Atividade Final/17 DijkstraHeap.py
@@ -1,17 +1,24 @@
 from collections import defaultdict
 from heapq import *
 
+# Dijkstra usando heap
 
-def dijkstra(arcos, f, t, D=None):
+
+def dijkstra(arcos, s, t, D=None):
     D = defaultdict(list)
     for l, r, c in arcos:
         D[l].append((c, r))
 
-    q, visitados, minimo = [(0, f, ())], set(), {f: 0}
-    while q:
-        (w, v, caminho) = heappop(q)
+    heap, visitados, minimo = [(0, s, ())], set(), {s: 0}
+
+    while heap:
+        (w, v, caminho) = heappop(heap)
+        print('Custo caminho total', w)
+        print('Vertice atual ', v)
+        print('Caminmho: ', caminho)
         if v not in visitados:
             visitados.add(v)
+            print('Visitando {}'.format(v))
             caminho = (v, caminho)
             if v == t:
                 return (w, caminho)
@@ -19,32 +26,32 @@ def dijkstra(arcos, f, t, D=None):
             for c, v2 in D.get(v, ()):
                 if v2 in visitados:
                     continue
-                anterior = minimo.get(v2, None)
+                predecessor = minimo.get(v2, None)
+
                 prox = w + c
-                if anterior is None or prox < anterior:
+                print('Distancia total para {} = {}'.format(v2, prox))
+                if predecessor is None or prox < predecessor:
                     minimo[v2] = prox
-                    heappush(q, (prox, v2, caminho))
+                    print('Mais proximo?', prox)
+                    heappush(heap, (prox, v2, caminho))
 
     return float("inf")
 
 
-if __name__ == "__main__":
-    edges = [
-        ("A", "B", 7),
-        ("A", "D", 5),
-        ("B", "C", 8),
-        ("B", "D", 9),
-        ("B", "E", 7),
-        ("C", "E", 5),
-        ("D", "E", 15),
-        ("D", "F", 6),
-        ("E", "F", 8),
-        ("E", "G", 9),
-        ("F", "G", 11)
-    ]
+edges = [
+    ("A", "B", 7),
+    ("A", "D", 5),
+    ("B", "C", 8),
+    ("B", "D", 9),
+    ("B", "E", 7),
+    ("C", "E", 5),
+    ("D", "E", 15),
+    ("D", "F", 6),
+    ("E", "F", 8),
+    ("E", "G", 9),
+    ("F", "G", 11)
+]
 
-    print("=== Dijkstra ===\n\n")
+print("=== Dijkstra ===\n\n")
 
-    print('Caminho mínimo de A a E', dijkstra(edges, "A", "E"))
-
-    print('Caminho mínimo de F a G', dijkstra(edges, "F", "G"))
+print('Caminho mínimo de A a E', dijkstra(edges, "A", "E"))
