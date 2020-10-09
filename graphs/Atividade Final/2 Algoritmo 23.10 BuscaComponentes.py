a37baf9682e534f983b4995abdf1f0e5852fb2be
gleydsonbrito <gleydsonbrito@hotmail.com>
2020-10-09 17:15:27 -0300

    Algoritmo 23.10: BuscaComponentes(G)

---
+++ graphs/Atividade Final/2 Algoritmo 23.10 BuscaComponentes.py
@@ -0,0 +1,57 @@
+grafo = {
+    1: {2, 3},
+    2: {1, 3, 4, 8, 11},
+    3: {1, 2, 4, 5},
+    4: {2, 3, 8, 7, 9},
+    5: {3, 6},
+    6: {5},
+    7: {4, 10, 11},
+    8: {2, 4},
+    9: {4, 11},
+    10: {4, 11},
+    11: {7, 9, 10},
+    12: {13},
+    13: {14, 15, 16},
+    14: {13, 17, 19},
+    15: {13, 17, 18, 19},
+    16: {13, 15, 20},
+    17: {14, 15, 19},
+    18: {15},
+    19: {14, 15, 17, 20},
+    20: {16, 19}}
+
+
+def quantidadeComponentes(componentes):
+    return print('Total de componentes conexos: ', len(componentes))
+
+
+def grupos(G):
+    visitado = set()
+    resultado = []
+    for v in G:
+        if v not in visitado:
+            print('{} ainda não foi visitado'.format(v))
+            conectados, visitado = gruposConectados(
+                v, visitado)
+
+            print('O componente {} está no componente {}'.format(
+                v, conectados))
+            resultado.append(conectados)
+    return resultado
+
+
+def gruposConectados(v, visitado):
+    resultado = []
+    vertices = set([v])
+    print('Visitando {} e verificando suas conexões'.format(v))
+    while vertices:
+        v = vertices.pop()
+
+        visitado.add(v)
+        vertices = vertices or grafo[v] - visitado
+
+        resultado.append(v)
+    return resultado, visitado
+
+
+quantidadeComponentes(grupos(grafo))
