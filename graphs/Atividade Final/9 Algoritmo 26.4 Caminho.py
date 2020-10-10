54a16ee0b99bf0c8aa49a64ef68e354569f32e3f
gleydsonbrito <gleydsonbrito@hotmail.com>
2020-10-10 15:10:15 -0300

    Algoritmo 26.4: Caminho(D, w, i, j, X)

--- graphs/Atividade Final/9 Algoritmo 26.4 Caminho.py
+++ graphs/Atividade Final/9 Algoritmo 26.4 Caminho.py
@@ -1,14 +1,29 @@
-grafo = {'A': ['B', 'C'],
-         'B': ['C', 'D'],
-         'C': ['D', 'E', 'G'],
-         'D': ['C', 'A', 'D'],
-         'E': ['F', 'C', 'D', 'A'],
-         'F': ['C', 'E', 'D']}
-
+grafo = {
+    1: {2, 3},
+    2: {1, 3, 4, 8, 11},
+    3: {1, 2, 4, 5},
+    4: {2, 3, 8, 7, 9},
+    5: {3, 6},
+    6: {5},
+    7: {4, 10, 11},
+    8: {2, 4},
+    9: {4, 11},
+    10: {4, 11},
+    11: {7, 9, 10},
+    12: {13},
+    13: {14, 15, 16},
+    14: {13, 17, 19},
+    15: {13, 17, 18, 19},
+    16: {13, 15, 20},
+    17: {14, 15, 19},
+    18: {15},
+    19: {14, 15, 17, 20},
+    20: {16, 19}}
 # função que encontra o caminho entre dois vertices
 
 
-def encontra_caminho(D, v_inicial, v_final, caminhos=[]):
+def encontra_caminho(D, v_inicial, v_final, p, caminhos=[]):
+    p += 1
     print('Encontrar o caminho de {} a {}'.format(v_inicial, v_final))
     # adiciona o vertice inicial como primeiro elemento do caminho
     caminhos = caminhos + [v_inicial]
@@ -22,12 +37,14 @@ def encontra_caminho(D, v_inicial, v_final, caminhos=[]):
 
     # percorre todos os vertices para identificar se há caminho entre eles
     for v in D[v_inicial]:
+        p += 1
         if v not in caminhos:
             # se há caminho faz uma chamada recursiva a partir desse vértice
-            newpath = encontra_caminho(D, v, v_final, caminhos)
+            newpath = encontra_caminho(D, v, v_final, p, caminhos)
             if newpath:
                 return newpath
+        print('Passos: {}'.format(p))
     return 'Não há caminho'
 
 
-print('Caminho: ', encontra_caminho(grafo, 'E', 'D'))
+print('Caminho: ', encontra_caminho(grafo, 1, 9, 0))
