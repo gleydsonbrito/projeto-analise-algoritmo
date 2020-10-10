ea5ef3d9e456ffe5ae66b8cca08444a6b75b40f8
gleydsonbrito <gleydsonbrito@hotmail.com>
2020-10-10 15:02:18 -0300

    Algoritmo 26.1: Dijkstra(D, w, s)

--- graphs/Atividade Final/8 Algoritmo 26.1 DijkstraDigrafo.py
+++ graphs/Atividade Final/8 Algoritmo 26.1 DijkstraDigrafo.py
@@ -16,15 +16,17 @@ class Digrafo(object):
         self.distancia[(u, v)] = dist
 
 
-def dijkstra(D, s):
+def dijkstra(D, s, p):
     visitado = {s: 0}
     path = {}
 
     V = set(D.vertice)
     print('Inicia pelo vertice {}'.format(s))
     while V:
+        p += 1
         minimo = None
         for vert in V:
+            p += 1
             print('Verifica se {} já foi visitado ?'.format(vert))
             if vert in visitado:
                 print('Visitando {}'.format(vert))
@@ -42,6 +44,7 @@ def dijkstra(D, s):
         print("Peso total {}".format(visitado[minimo]))
 
         for y in D.arcos[minimo]:
+            p += 1
             try:
                 peso = peso_atual + D.distancia[(minimo, y)]
             except:
@@ -49,38 +52,38 @@ def dijkstra(D, s):
             if y not in visitado or peso < visitado[y]:
                 visitado[y] = peso
                 path[y] = minimo
-
+    print('Passos: {}'.format(p))
     return visitado, path
 
 
-def caminho_minimo(D, s, destino):
-    visitado, paths = dijkstra(D, s)
+def caminho_minimo(D, s, destino, p):
+    visitado, paths = dijkstra(D, s, p)
     caminho_total = deque()
     _destino = paths[destino]
     print('Caminho de {} até {}'.format(s, destino))
 
     while _destino != s:
+        p += 1
         caminho_total.appendleft(_destino)
         _destino = paths[_destino]
 
     caminho_total.appendleft(s)
     caminho_total.append(destino)
-
     return visitado[destino], list(caminho_total)
 
 
 dig = Digrafo()
 
-for v in ['A', 'B', 'C', 'D', 'E', 'F', 'G']:
+for v in [1, 2, 3, 4, 5, 6, 7]:
     dig.adicionaVertice(v)
 
-dig.adicionaArco('A', 'B', 10)
-dig.adicionaArco('A', 'C', 20)
-dig.adicionaArco('B', 'D', 15)
-dig.adicionaArco('C', 'D', 30)
-dig.adicionaArco('B', 'E', 50)
-dig.adicionaArco('D', 'E', 30)
-dig.adicionaArco('E', 'F', 5)
-dig.adicionaArco('F', 'G', 2)
+dig.adicionaArco(1, 2, 10)
+dig.adicionaArco(1, 3, 20)
+dig.adicionaArco(2, 4, 15)
+dig.adicionaArco(3, 4, 30)
+dig.adicionaArco(2, 5, 50)
+dig.adicionaArco(4, 5, 30)
+dig.adicionaArco(5, 6, 5)
+dig.adicionaArco(6, 7, 2)
 
-print('Custo mínimo: ', caminho_minimo(dig, 'A', 'E'))
+print('Custo mínimo: ', caminho_minimo(dig, 1, 5, 0))
