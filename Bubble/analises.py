51f945be17ef65e0b0f68fd8c51969a81af1a652
gleydsonbrito <gleydsonbrito@hotmail.com>
2020-10-15 13:12:32 -0300

    analises dos algorimtos em grupo de discussão no meet

---
+++ Bubble/analises.py
@@ -0,0 +1,82 @@
+import random
+
+
+def bubbleSort0(A, d=0):  # Melhor caso O(n) e Pior caso O(n²)
+    # Super ingênuo
+    n = len(A)-1
+    for i in range(n):
+        for j in range(n):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+
+
+def bubbleSort1(A, d=0):
+    # bubble naive mas nem tanto
+    n = len(A)-1
+    for i in range(n):  # O(n)
+        for j in range(0, n-i):
+            if A[j] > A[j+1]:
+                aux = A[j]
+                A[j] = A[j+1]
+                A[j+1] = aux
+
+
+def bubbleSort2(A, d=0):
+    # bubble com flag sem considerar parte ordenada
+    n = len(A)
+    for i in range(n-1):
+        flag = 0
+        for j in range(n-1):
+            if A[j] > A[j+1]:
+                aux = A[j]
+                A[j] = A[j+1]
+                A[j+1] = aux
+                flag = 1
+        if flag == 0:
+            break
+
+
+def bubbleSort3(A, d=0):
+    # bubble com flag considerando parte ordenada de forma naive
+    n = len(A)
+    for i in range(n-1):
+        flag = 0
+        for j in range(n-1):
+            if A[j] > A[j+1]:
+                aux = A[j]
+                A[j] = A[j+1]
+                A[j+1] = aux
+                flag = 1
+        n -= 1  # otimização
+        if flag == 0:
+            break
+
+
+def bubbleSort4(A, d=0):
+    # considerando parte já ordenada de forma inteligente
+    n = len(A)
+    while True:
+        newn = 0
+        for j in range(n-1):
+            if A[j] > A[j+1]:
+                aux = A[j]
+                A[j] = A[j+1]
+                A[j+1] = aux
+                newn = j
+        n = newn+1
+        if n <= 1:
+            break
+
+
+def bubbleSort5(A, d=0):
+    # sem usar break mas naive como o 2,
+    # pode ser melhorado usando técnicas do 1 ou do 4
+    n = len(A) - 1
+    ordenado = False
+    while not ordenado:
+        ordenado = True
+        for i in range(n):
+            if A[i] > A[i + 1]:
+                A[i], A[i + 1] = A[i + 1], A[i]
+                ordenado = False
+
--- Bubble/bubbles.py
+++ Bubble/bubbles.py
@@ -46,6 +46,51 @@ def bubbleSort1(A, d=0):
                 A[j+1] = aux
     print(s, " passos")
 
+# Em todos os algoritmos o parametro d é de debug naive
+# d=0, sem debug. d=1, com.
+
+
+def bubbleSort0(A, d=0):
+    # Super ingênuo
+    n = len(A)-1
+    s = 0
+    c = " "
+    for i in range(n):
+        for j in range(n):
+            s += 1
+            if d:
+                print(A)
+            if A[j] > A[j+1]:
+                if d:
+                    m = (2*c)+"|"+(2*c)
+                    l = (j)*(4*c)+m+(n-j-len(m))*(4*c)
+                    print(l)
+                    print(j)
+                A[j], A[j+1] = A[j+1], A[j]
+    print(s, " passos")
+
+
+def bubbleSort1(A, d=0):
+    # bubble naive mas nem tanto
+    n = len(A)
+    s = 0
+    c = " "
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            s += 1
+            if d:
+                print(A)
+            if A[j] > A[j+1]:
+                if d:
+                    m = (2*c)+"|"+(2*c)
+                    l = (j)*(4*c)+m+(n-j-len(m))*(4*c)
+                    print(l)
+                    print(j)
+                aux = A[j]
+                A[j] = A[j+1]
+                A[j+1] = aux
+    print(s, " passos")
+
 
 def bubbleSort2(A, d=0):
     # bubble com flag sem considerar parte ordenada
@@ -94,9 +139,9 @@ def bubbleSort3(A, d=0):
                 A[j] = A[j+1]
                 A[j+1] = aux
                 flag = 1
-            n -= 1
-            if flag == 0:
-                break
+        n -= 1
+        if flag == 0:
+            break
     print(s, " passos")
 
 
@@ -121,8 +166,8 @@ def bubbleSort4(A, d=0):
                 A[j] = A[j+1]
                 A[j+1] = aux
                 newn = j
-        n = newn
-        if n < 1:
+        n = newn+1
+        if n <= 1:
             break
     print(s, " passos")
 
